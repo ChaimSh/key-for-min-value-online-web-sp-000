0d4b1ea9b186d8d5a85c31ecd7885fb00b8242c0
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-04-02 02:09:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,7 +14,7 @@ def key_for_min_value(name_hash)
   lowest = nil
  name = nil
  name_hash.each do |key, value|
-    if lowest == nil 
+    if lowest == nil
       lowest = value
       name = key
     end
